115641cf7a89550a03cd4fbe430edc59f713a9fa
mlnlsTER <99759208+mlnlsTER@users.noreply.github.com>
2022-12-03 14:06:06 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,93 +1,80 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
-int * arr; //Глобальный массив - условие
+int value = 0;
 
-void * attr_thread_function(void * thread_arg) { //Выводим постоянно элементы
-    int i, n = *(int*) thread_arg;
-
-    while(1)
-    {
-        sleep(1); //Спим 1 секунду - условие
-
-        for (i = 0; i < n; i++) //Выводим массив
-        {
-            fprintf(stderr, "%d ", arr[i]);
-        }
-
-        fprintf(stderr, "\n");
-
-        if (arr[n - 1]) //Если массив заполнен - выйти из цикла, закончить поток. Условие
-        {
-            break;
-        }
-    }
-    
-    return NULL;
+void * incrementI(void * arg) { 
+	 int val = *(int*) arg;
+     int i;
+     for (i = 0; i < val; i++)
+     	value++;	
+     srand(time(NULL));
+     int r = rand() % 5 + 1;
+     sleep(r);
+     pthread_exit(NULL);
 }
 
-void * thread_function(void * arg) //Заполняем массив
-{
-	int n = *(int*) arg; //Принимаем целочисленное значение из аргумента
-
-	sleep(n); //Спим столько секунд сколько необходимо - условие
-    arr[n] = rand() % 10 + 1; //Рандомное число от 1 до 10 - условие
-
-    sleep(arr[n]); //Спим столько секунд, сколько нарандомили - условие
-
-	return NULL;
+void * decrementI(void * arg) { 
+	 int val = *(int*) arg;
+     int i;
+     for (i = 0; i < val; i++)
+     	value--;	
+     srand(time(NULL));
+     int r = rand() % 5 + 1;
+     sleep(r);
+     pthread_exit(NULL);
 }
 
-int create_threads(const int n) //Функция создания потоков
-{
-    int i;
-    pthread_t * threads = malloc(sizeof(pthread_t) * n); //Выделяем память потокам
-    
-    for (i = 0; i < n; i++)
-    {
-        if(pthread_create(&threads[i], NULL, &thread_function, &i) != 0) //Создаем столько потоков сколько надо
-	    {
-		    fprintf(stderr, "Error\n");
-		    return 1;
-	    }
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        if(pthread_join(threads[i], NULL) != 0) //Ждём завершения каждого созданного потока
-	    {
-		    fprintf(stderr, "Error\n");
-		    return 1;
-	    }
-    }
-
-    return 0;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) //Если аргументов в командной строке мало то ошибка
-	{
-		fprintf(stderr, "Too few arguments\n");
-		return 1;
+int main(int argc, char **argv){
+	pthread_t *p_thread_1, *p_thread_2;
+	int th = 10;
+	int val = 3;
+	char *opts = "t:v:"; 
+	int opt; 
+	while((opt = getopt(argc, argv, opts)) != -1) {
+		switch(opt) {
+			case 't': 
+				th = atoi(optarg);
+				break;
+			case 'v': 
+				val = atoi(optarg);
+				break;	
+		}
 	}
-    int i, n = atoi(argv[1]); //Считываем то что написали в консольке
-    arr = malloc(sizeof(int) * n); //Выделяем массиву память
 
-    srand(time(NULL));
 
-    pthread_attr_t attr; //Инициализируем атрибут
-    pthread_t attr_thread; //Поток с атрибутом
+	p_thread_1 = malloc (sizeof(pthread_t) * th/2);
+	p_thread_2 = malloc (sizeof(pthread_t) * th/2);
+	int i;
 
-    pthread_attr_init (&attr); //Инициализирует атрибут
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED); //Говорим шо он (атрибут) делает поток отсоединенным
 
-    pthread_create(&attr_thread, &attr, &attr_thread_function, &n); //Создаем отсоединенный поток (не нужно ждать его завершения в мейне)
-    create_threads(n); //Создаем потоки через функцию - условие
+	for (i = 0; i < th/2; ++i){
+		if (pthread_create(&p_thread_1[i], NULL, &incrementI, &val) != 0) {
+			fprintf(stderr, "Ошибка (поток 1)\n");
+			return 1;
+		}
+		if (pthread_create(&p_thread_2[i], NULL, &decrementI, &val) != 0) {
+			fprintf(stderr, "Ошибка (поток 2)\n");
+			return 1;
+		}		
+	}
 
-    pthread_attr_destroy (&attr); //Уничтожаем атрибут
-    
-    return 0;
+		
+	for(i = 0; i < th/2; ++i){
+		if (pthread_join(p_thread_1[i], NULL) != 0) {
+			printf("Ошибка\n");		
+		}
+		if (pthread_join(p_thread_2[i], NULL) != 0) {
+			printf("Ошибка\n");		
+		}
+	}
+
+
+	printf("Value: %d\n", value);
+	free(p_thread_1);
+	free(p_thread_2);
+	return 0;
 }
