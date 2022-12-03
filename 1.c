267296a564705c5544145312d49d5851414ca015
mlnlsTER <99759208+mlnlsTER@users.noreply.github.com>
2022-12-03 14:11:12 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,80 +1,95 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <unistd.h>
-#include <time.h>
 #include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-int value = 0;
+void *consumer_thread(void *arg);
+void *conductor_thread(void *arg);
 
-void * incrementI(void * arg) { 
-	 int val = *(int*) arg;
-     int i;
-     for (i = 0; i < val; i++)
-     	value++;	
-     srand(time(NULL));
-     int r = rand() % 5 + 1;
-     sleep(r);
-     pthread_exit(NULL);
+sem_t conductor_sem;
+sem_t consumer_sem;
+sem_t complite_sem;
+
+int *arr, currentN = 0, n, min, max;
+
+#define WORK_SIZE 1024
+char work_area[WORK_SIZE];
+
+int main(int argc, char *argv[]) {
+    srand(time(NULL));
+    if (argc < 5) //Если аргументов в командной строке мало то ошибка
+	{
+		fprintf(stderr, "Too few arguments\n");
+		return 1;
+	}
+    int seconds = atoi(argv[1]);
+    n = atoi(argv[2]); min = atoi(argv[3]); max = atoi(argv[4]);
+    arr = malloc(sizeof(int) * n);
+
+    pthread_t conductor, consumer;
+    void *thread_result;
+
+    sem_init(&conductor_sem, 1, 0);
+    sem_init(&consumer_sem, 1, n);
+    sem_init(&complite_sem, 1, 1);
+
+    pthread_create(&consumer, NULL, consumer_thread, NULL);
+    pthread_create(&conductor, NULL, conductor_thread, NULL);
+
+    sleep(seconds);
+
+    pthread_cancel(conductor);
+    pthread_cancel(consumer);
+
+    exit(EXIT_SUCCESS);
 }
 
-void * decrementI(void * arg) { 
-	 int val = *(int*) arg;
-     int i;
-     for (i = 0; i < val; i++)
-     	value--;	
-     srand(time(NULL));
-     int r = rand() % 5 + 1;
-     sleep(r);
-     pthread_exit(NULL);
+void *consumer_thread(void *arg) {
+    while (1) {
+        sleep(2);
+        sem_wait(&conductor_sem);
+        sem_wait(&complite_sem);
+
+        arr[currentN] = 0;
+        currentN--;
+
+        sem_post(&complite_sem);
+        sem_post(&consumer_sem);
+        
+        int i;
+        for (i = 0; i < currentN; i++) {
+            fprintf(stderr, "%d ", arr[i]);
+        }
+
+        fprintf(stderr, " - Consume\n");
+    }
+
+    pthread_exit(NULL);
 }
 
-int main(int argc, char **argv){
-	pthread_t *p_thread_1, *p_thread_2;
-	int th = 10;
-	int val = 3;
-	char *opts = "t:v:"; 
-	int opt; 
-	while((opt = getopt(argc, argv, opts)) != -1) {
-		switch(opt) {
-			case 't': 
-				th = atoi(optarg);
-				break;
-			case 'v': 
-				val = atoi(optarg);
-				break;	
-		}
-	}
+void *conductor_thread(void *arg) {
+    while (1) {
+        sleep(1);
+        int numb = rand() % (max + min) - min; 
 
+        sem_wait(&consumer_sem);
+        sem_wait(&complite_sem);
 
-	p_thread_1 = malloc (sizeof(pthread_t) * th/2);
-	p_thread_2 = malloc (sizeof(pthread_t) * th/2);
-	int i;
+        arr[currentN] = numb;
+        currentN++;
 
+        sem_post(&conductor_sem);
+        sem_post(&complite_sem);
 
-	for (i = 0; i < th/2; ++i){
-		if (pthread_create(&p_thread_1[i], NULL, &incrementI, &val) != 0) {
-			fprintf(stderr, "Ошибка (поток 1)\n");
-			return 1;
-		}
-		if (pthread_create(&p_thread_2[i], NULL, &decrementI, &val) != 0) {
-			fprintf(stderr, "Ошибка (поток 2)\n");
-			return 1;
-		}		
-	}
+        int i;
+        for (i = 0; i < currentN; i++) {
+            fprintf(stderr, "%d ", arr[i]);
+        }
 
-		
-	for(i = 0; i < th/2; ++i){
-		if (pthread_join(p_thread_1[i], NULL) != 0) {
-			printf("Ошибка\n");		
-		}
-		if (pthread_join(p_thread_2[i], NULL) != 0) {
-			printf("Ошибка\n");		
-		}
-	}
+        fprintf(stderr, " - Conduct\n");
+    }
 
-
-	printf("Value: %d\n", value);
-	free(p_thread_1);
-	free(p_thread_2);
-	return 0;
+    pthread_exit(NULL);
 }
